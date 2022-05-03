68bf92049074178fa41142f6db57eac6b115e657
Ricardo-we <totgamer75@gmail.com>
2022-05-03 16:19:17 -0600

    Added pg to dialect module in Sequelize init

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
 import { config } from 'dotenv';
+import * as pg from 'pg';
 
 config();
 
-export const sequelize = new Sequelize(process.env.DB_URL);
+export const sequelize = new Sequelize(process.env.DB_URL, {
+    dialectModule: pg
+});
